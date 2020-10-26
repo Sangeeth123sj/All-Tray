063ce1fd39608b5d538f395d70ee2cd7bcd3940c
Sangeeth Joseph <sangeeth123sj#gmail.com>
2020-10-26 23:17:45 +0530

    link user to store

--- tray/models.py
+++ tray/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import Model
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Student(models.Model):
@@ -17,6 +18,8 @@ class Item(models.Model):
 class Store(models.Model):
     store_name = models.CharField(max_length=200)
     store_status = models.BooleanField()
+    store_balance = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.store_name)+" "+ str(self.store_status)
