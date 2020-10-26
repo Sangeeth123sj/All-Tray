d45db2522881bc2151b6b81d4dbbe389cdfd31a8
Sangeeth Joseph <sangeeth123sj#gmail.com>
2020-10-26 09:38:13 +0530

    add store login and modify registering store

--- tray/urls.py
+++ tray/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('order_items/', views.order_items, name = 'order_items'),
     path('register_store/', views.open_store, name = 'open_store'),
     path('store_home/<int:store_id>/', views.store_home, name = 'store_home'),
+    path('register_test/', views.user_reg, name = 'user_reg'),
+    path('login/', views.store_login, name = 'store_login'),
+    path('login_verify/', views.store_login_processing, name = 'store_login' )
 ]
--- tray/views.py
+++ tray/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Student,Item,Store
-from tray.forms import OrderForm
+from django.contrib.auth.models import User, Permission
+from django.contrib.auth import authenticate, login
+
 # Create your views here.
 
 def index(request, name_id):
@@ -38,6 +40,11 @@ def open_store(request):
 def store_home(request, store_id):
     if request.method == 'POST':
         name = request.POST['store_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = User.objects.create_user(username,email,password)
+        user.save()
         s = Store(store_name= name, store_status = True)
         s.save()
         status = s.store_status
@@ -56,3 +63,27 @@ def store_home(request, store_id):
             c = "Inactive"
 
     return render(request, 'tray/store_home.html',{'name':name, 'status':c})
+
+def store_login(request):
+    return render(request, 'tray/login_store.html' )
+
+
+def store_login_processing(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username = username, password = password)
+
+    if user is not None:
+        login(request, user)
+        return render(request, 'tray/store_home.html')
+    else:
+        c = "Sorry login failed!"
+        return HttpResponse(c)
+    
+    
+
+def user_reg(request):
+    user = User.objects.create_user('sanga','sangeeth@gmail.com','123')
+    user.save()
+    return HttpResponse("user sang created")
+
