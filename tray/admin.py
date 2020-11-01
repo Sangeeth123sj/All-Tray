996fe1698f0393c9a22c7b02561e7e5d7f9376fd
Sangeeth Joseph <57926468+Sangeeth123sj@users.noreply.github.com>
2020-11-01 15:08:50 +0530

    add student order items with pickup time/break option and manyToOne relation to student (#2)
    Co-authored-by: Sangeeth Joseph <sangeeth123sj#gmail.com>

--- tray/admin.py
+++ tray/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Student, Item, Store
+from .models import Student, Item, Store, Order
 admin.site.register(Student)
 admin.site.register(Item)
 admin.site.register(Store)
+admin.site.register(Order)
 # Register your models here.
--- tray/models.py
+++ tray/models.py
@@ -3,6 +3,14 @@ from django.db.models import Model
 from django.contrib.auth.models import User
 # Create your models here.
 
+BREAKS = (  
+    ('A', 'Now'),
+    ('B', 'First Break'),
+    ('C', 'Second Break'),
+    ('D', 'LAst Break'),
+)
+
+
 class Student(models.Model):
     name = models.CharField(max_length=200)
     reg_no = models.IntegerField(default=0)
@@ -13,11 +21,19 @@ class Store(models.Model):
     store_status = models.BooleanField()
     store_balance = models.IntegerField(default=0)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
+    
     def __str__(self):
         return str(self.store_name)+" "+ str(self.store_status)
 
 class Item(models.Model):
     item = models.CharField(max_length=200)
     quantity = models.IntegerField(default=1)
+    prize = models.IntegerField(default=0)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    item1 = models.CharField(max_length=200, blank = True)
+    quantity1 = models.IntegerField(default=0)
+    cost1 = models.IntegerField(default=0)
+    Time = models.CharField(max_length=200, choices=BREAKS)
+    Student = models.ForeignKey(Student, on_delete=models.CASCADE)
